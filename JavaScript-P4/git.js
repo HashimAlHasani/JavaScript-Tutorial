7cae940d3b0728b673e8d7e987f7a1378926eaa9
Tomiisme <Hashim.alhassani@outlook.com>
2023-12-12 02:29:43 +0300

    Added JavaScript-p4

---
+++ JavaScript-P4/git.js
@@ -0,0 +1,6 @@
+(function(){
+    //IIFE - immediately invoked function expression
+    var age = 5;
+    //if we don't have the var keyword it will look for the "age" variable from the parent or outer scope.
+})(); //This () tells it to execute
+//This set of () says that this is an expression and not just a function declaration (the bracket starting before the word function)
