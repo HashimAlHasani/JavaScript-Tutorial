4c50c4becfc22a1dba7fc3d00921ad00bc145c8b
Tomiisme <Hashim.alhassani@outlook.com>
2024-01-26 01:39:15 +0300

    Added JavaScript-P49

---
+++ JS-Parts/JavaScript-P49/git.js
@@ -0,0 +1,15 @@
+/*
+An object will describe some entity (can be anything).
+
+A constructor function takes some information as input and gives us an object.
+
+In JS there is no real difference between classes and constructor functions when 
+it comes to functionality and the way they are used, they are different when
+it comes to syntax and how you actually make them.
+
+The constructor function is going to use this keyword - when you use the this keyword 
+inside of a constructor function it is refers to the new object you are creating.
+
+The constructor function is not going to have a return, instead you`ll going 
+to invoke this function in a special way by prefixing it with "new" - let person = new Person();
+*/
